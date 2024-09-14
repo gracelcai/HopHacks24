c11e8424f2cab34d89d3307bbdfe2c2fe2a4f964
Grace Cai <grace.cai7@gmail.com>
2024-09-14 19:44:57 -0400

    added finetuning

--- analyzer.py
+++ analyzer.py
@@ -8,6 +8,11 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
+client.fine_tuning.jobs.create(
+  training_file="/data.json", 
+  model="gpt-4o-mini"
+)
+
 def get_results(text, option):
     messages = [{"role": "system", "content": f"You are a helpful assistant that evaluates text messages based on categories to decide whether the receiver should be notified. "}]
     messages.append({"role": "user", "content": f"Given the following text message: \"{text}\", give percentages for each of the following grading categories below:"})
