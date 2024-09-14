2b78d97e824e4c9deafccc854e041b7b823559f2
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-09-14 11:30:33 -0400

    return res

--- helper.py
+++ helper.py
@@ -60,7 +60,8 @@ def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=N
                    channels="BGR",
                    use_column_width=True
                    )
-
+    
+    return res
 
 def play_rtsp_stream(conf, model):
     """
