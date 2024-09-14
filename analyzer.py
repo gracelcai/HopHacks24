6e2304e40194faef650be28e5bed12b23ecec287
Grace Cai <grace.cai7@gmail.com>
2024-09-14 17:11:23 -0400

    openai is mean for making me pay

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 import os
 import json
+from trying_random_bs import *
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 print(f"key:{OPENAI_API_KEY}")
@@ -9,18 +10,20 @@ client = OpenAI(api_key=OPENAI_API_KEY)
 def load_messages():
     messages = [{"role": "system", "content": "You are a helpful assistant that evaluates text messages based on several categories to decide whether the receiver should be notified about it or not."}]
     with open('defs.json', 'r') as file:
-        data = json.load(file).items()
-        for word, definition in data["categories"]:
+        data = json.load(file)
+        print(data)
+        for word, definition in data["categories"].items():
             messages.append({"role": "user", "content": "Here is the definition for texts that fall under the category of {word}: {definition}"})
-        for word, definition in data["modes"]:
+        for word, definition in data["modes"].items():
             messages.append({"role": "user", "content": "Here is the definition for the mode {word}: {definition}"})
     return messages
 
 def get_results(text):
     messages = load_messages()
     messages.append({"role": "user", "content": "Here is the text message to be analyzed: {text}"})
+    messages.append({"role": "user", "content": prompt})
     completion = client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4o-mini",
         messages=messages
     )
     return completion
