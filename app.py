710db0ccd59c1fd3200eedb200de41d8cedbcfe1
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-09-14 19:42:23 -0400

    almost fixed

--- app.py
+++ app.py
@@ -18,17 +18,13 @@ text = st.text_area("Please Enter your text")
 if st.button("Analyze the Sentiment"): 
   blob = TextBlob(text) 
   result = analyzer.get_results(blob, option)
+    
   message_content = result.choices[0].message.content
   content_trimmed = message_content[7:-3]
   message_json = json.loads(content_trimmed)
-  st.write(message_json)
+  
+  st.write(f"Sent: {message_json["notify"]["yes"]}")
 
-  st.write(message_json["urgency"])
-
-  # message_content = result.choices[0].message['content']
-  # # dic = result.choices[0].message.content
-
-  # jsonresult = json.loads(message_content)
-  # st.write(type(jsonresult))
-  # # for category, definition in data["categories"].items():
-  #   # st.progress(result.choices[0].message.content[0], text=f"{category}")
+  for category, definition in data["categories"].items():
+    percentage = message_json[f"{category}"]
+    st.progress(percentage, text=f"{category}: {percentage}%")
