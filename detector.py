733e72e1cd3c2a39214cef4fa179a0bbe3e655f3
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-09-14 11:15:27 -0400

    change file name

--- detector.py
+++ detector.py
@@ -8,7 +8,7 @@ import streamlit as st
 # Local Modules
 import settings
 import helper
-import table
+import table_maker
 
 # Setting page layout
 st.set_page_config(
@@ -33,6 +33,7 @@ except Exception as ex:
     st.error(f"Unable to load model. Check the specified path: {model_path}")
     st.error(ex)
 
+st.table(table_maker.create())
 
 # st.sidebar.header("Image/Video Config")
 # source_radio = st.sidebar.radio(
