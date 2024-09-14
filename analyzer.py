4c40dd28927a0962887f5f8d0bfa0ad4dd531638
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-09-14 18:34:38 -0400

    update

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
-import os
 from dotenv import load_dotenv
 import json
+import os
 
 load_dotenv()
 
@@ -14,8 +14,8 @@ def get_results(text, option):
     
     file = open('defs.json', 'r')
     data = json.load(file)
-    for word, definition in data["categories"].items():
-        messages.append({"role": "user", "content": f"{word}: {definition}"})
+    for category, definition in data["categories"].items():
+        messages.append({"role": "user", "content": f"{category}: {definition}"})
     
     defn = data["modes"][option]
     messages.append({"role": "user", "content": f"With those percentages, should a user in mode: \"{option}\" ({defn}) be notified? return only a json file with each grading category and their respective percentages and also yes or no and its confidence"})
--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from textblob import TextBlob
 import analyzer
+import json
+
+file = open('defs.json', 'r')
+data = json.load(file)
 
 st.title("Text Message Sentiment Analysis") 
 st.text("This webapp evaluates whether the receiver of a text message should be notified based on the urgency of the message.")
@@ -14,4 +18,6 @@ text = st.text_area("Please Enter your text")
 if st.button("Analyze the Sentiment"): 
   blob = TextBlob(text) 
   result = analyzer.get_results(blob, option)
-  st.write(result)
+  
+  for category, definition in data["categories"].items():
+    st.progress(result.choices[0].message.content[0], text=f"{category}")
