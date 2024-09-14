9ef98dc9b4e70e7ae21e69f099b9407938d4651f
chrissu777 <christophersu777@gmail.com>
2024-09-14 18:23:56 -0400

    dotenv

--- analyzer.py
+++ analyzer.py
@@ -1,8 +1,11 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
 import json
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 def get_results(text, option):
