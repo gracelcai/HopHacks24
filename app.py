8255581afe8c81737eb591171c0099b7e32bbe8f
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2024-09-14 17:42:54 -0400

    add mode

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import analyzer
 
 st.title("Text Message Sentiment Analysis") 
 st.text("This webapp evaluates whether the receiver of a text message should be notified based on the urgency of the message.")
+
 option = st.selectbox(
      'Select Notification Mode',
      ('Meeting', 'Work', 'Social', 'Available'))
@@ -12,5 +13,5 @@ text = st.text_area("Please Enter your text")
 
 if st.button("Analyze the Sentiment"): 
   blob = TextBlob(text) 
-  result = analyzer.get_results(blob)
+  result = analyzer.get_results(blob, option)
   st.write(result)
