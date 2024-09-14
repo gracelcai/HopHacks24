852c79b338e06af2ff4ddf99771359e7c6e17464
chrissu777 <christophersu777@gmail.com>
2024-09-14 17:31:45 -0400

    dropdown

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import analyzer
 
 st.title("Text Message Sentiment Analysis") 
 st.text("This webapp evaluates whether the receiver of a text message should be notified based on the urgency of the message.")
-  
+option = st.selectbox(
+     'Select Notification Mode',
+     ('Meeting', 'Work', 'Social', 'Available'))
+
 text = st.text_area("Please Enter your text")
 
 if st.button("Analyze the Sentiment"): 
